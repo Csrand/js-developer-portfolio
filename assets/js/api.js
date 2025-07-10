bd877dbdb892b36170910643cbf062fa5241df07
Csrand <chrystiansenna@gmail.com>
2025-07-10 13:36:00 -0300

    finalizando

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://github.com/Csrand/js-developer-portfolio/blob/main/assets/img/me.jpeg?raw=true'
+    const url = 'https://raw.githubusercontent.com/Csrand/js-developer-portfolio/refs/heads/main/data/profile.json'
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
